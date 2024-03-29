04bb90e2eacbbc640881da87bf47e93f55346cb8
Camila Pedraza <cami.pedraza29@gmail.com>
2024-03-28 19:26:47 -0500

    Home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,49 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Fluter + Material 3'),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItmes.length,
+      itemBuilder: (context, index){
+        final menuItem = appMenuItmes[index];
+        
+        return _CustomListTile(menuItem: menuItem);
+      }
+    
+  
+    );
+    
+
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary ),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){},
     );
   }
 }
